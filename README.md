287c4fd5dd9886999f9dfe3c127e28fd006aa16f
Mitchell Szczepanczyk <mitchells@opinionlab.com>
2013-05-07 05:33:49 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 wtdr
 ====
+# A surprisingly complicated Python program to test for two words that
+# don't rhyme.  Inspired by a game I play with my three-year-old son. 
+# 
+# Requires the cmudict file from the Natural Language Toolkit (NLTK) residing in the present working directory.
+
---
+++ words_that_dont_rhyme.py
@@ -0,0 +1,54 @@
+# A surprisingly complicated Python program to test for two words that
+# don't rhyme. 
+# 
+# Requires the cmudict file from the Natural Language Toolkit (NLTK) residing in the present working directory.
+
+class WordsThatDontRhyme():
+
+     def __init__(self):
+          self.vowels = ['AA', 'AH', 'EH', 'IH', 'OW', 'UH', 'AE', 'AO', 'AY', 'IY', 'ER', 'OY', 'UW']
+          lines = open('cmudict', 'r')
+          self.w = {} # words
+          self.s = () # selection
+          for line in lines:
+               t = line[:-1].split(' ')
+               try:
+                    self.w[t[0]].append(t[2:])
+               except: 
+                    self.w[t[0]] = [t[2:]]
+
+     def get_last_syllable_nucleus_coda(self, w):
+          to_return = [] 
+          for r in self.w[w]:
+               temp = []
+               r.reverse()
+               for e in r:
+                    temp.append(e)
+                    if e[0:2] in self.vowels:
+                          break
+               temp.reverse()
+               to_return.append(temp)
+          return to_return 
+
+     def check_rhyme(self):
+          s1 = self.get_last_syllable_nucleus_coda(self.s[0])
+          s2 = self.get_last_syllable_nucleus_coda(self.s[1])
+          for c1 in s1:
+               for c2 in s2:
+                    if c1 == c2:
+                         return True
+          return False
+
+     def select_words(self):
+           import random
+           self.s = random.sample(self.w, 2)
+
+     def main(self):
+          self.select_words()
+          while self.check_rhyme() == True:
+               self.select_words()
+          return self.s
+ 
+if __name__ == '__main__':
+     w = WordsThatDontRhyme() 
+     print w.main()
